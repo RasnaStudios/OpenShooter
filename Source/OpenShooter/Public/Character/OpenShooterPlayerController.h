38947f41327ececc74ead31f0e208eb7534a036f
Lorenzo Andraghetti <andraghetti.l@gmail.com>
2024-12-25 17:53:25 +0100

    Annoucement texts

--- Source/OpenShooter/Public/Character/OpenShooterPlayerController.h
+++ Source/OpenShooter/Public/Character/OpenShooterPlayerController.h
@@ -16,14 +16,18 @@ class OPENSHOOTER_API AOpenShooterPlayerController : public APlayerController
 {
     GENERATED_BODY()
 public:
+    UPROPERTY()
     AOpenShooterHUD* HUD;
 
     void SetHUDHealth(float Health, float MaxHealth);
     void SetHUDScore(float Score);
     void SetHUDDefeats(int32 Defeats);
+    void SetHUDAnnoucement(const FText& Message, float DisplayTime = 5.0f);
+    void HideAnnoucementText();
 
 protected:
     virtual void BeginPlay() override;
 
 private:
+    FTimerHandle HideAnnoucementTextTimerHandle;
 };
